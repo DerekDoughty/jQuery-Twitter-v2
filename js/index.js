a36581978d57d42e8aab65c0abd828cfc592ee6b
DerekDoughty <Doughty326@gmail.com>
2015-07-14 16:30:38 -0700

    twitter v2

---
+++ js/index.js
@@ -0,0 +1,16 @@
+'use strict';
+
+var currentUser = {
+  handle: '@bradwestfall',
+  img: 'brad.png',
+  id: 1
+};
+
+var $ = require ('jquery')
+
+$(function () {
+
+	console.log('whats up')
+
+
+});
---
+++ js/template.js
@@ -0,0 +1,4 @@
+'use strict';
+
+var Handlebars = require('hbsfy/runtime');
+var tweet = require('../templates/tweet.handlebars')
