9c870bcb7f30a074accec740f54e5249915ded8e
DerekDoughty <Doughty326@gmail.com>
2015-07-16 19:43:18 -0700

    added a large portion of JS for the project

--- js/index.js
+++ js/index.js
@@ -1,20 +1,83 @@
 'use strict';
+var $ = require ('jquery');
+var tmpl = require ('./template.js')
 
 var currentUser = {
-  handle: '@bradwestfall',
-  img: 'brad.png',
+  handle: '@DirtyD',
+  img: 'derekfire.jpg',
   id: 1
 };
 
-// var $ = require ('jquery');
+
+
+var usersUrl = 'http://localhost:3000/users'
+var tweetsUrl = 'http://localhost:3000/tweets'
+var repliesUrl = 'http://localhost:3000/replies'
+
+
+ function getUsers(){
+ 	console.log('hey')
+ 	return $.get(usersUrl)
+ 	.done(function (getUsersFromUserUrl){
+ 		console.log(getUsersFromUserUrl)
+ 	})
+ }
+getUsers()
+
+ function getTweets(){
+ 	console.log('tweets')
+ 	return $.get(tweetsUrl)
+ 	.done(function (getTweetsFromTweetsUrl){
+ 		console.log(getTweetsFromTweetsUrl)
+ 	})
+ }
+getTweets()
+
+
+	function renderCompose () {
+
+		return tmpl.compose()
+
+	}
 
 $(function () {
+	console.log('im loaded')
 
-	$('#main').on('click', 'textarea', function(){
+
+	$('#main').on('click', 'textarea', function () {
 		$(this).parent('form').toggleClass('expand')
+			console.log('whats up')
 	})
 
-	console.log('whats up')
+	$('#tweet').on('click', '.tweet', function () {
+		
+		$(this).parents('.thread').toggleClass('expand')
 
+	})
+
+
+	$('main').on('click', 'button', function () {
+		var message = $(this).parents('.compose').find('textarea').val()
+
+		if ($(this).parents('.replies').length) {
+			console.log($(this))
+			$(this).parents('.replies').append(renderTweet(User, message))
+		
+		} else {
+		
+			$('.tweets').append(renderThread(User, message))
+			
+		}
+
+		$(this).parents('.compose').removeClass('expand')
+
+		$('textarea').val('')
+		// console.log(renderThread(User, message))
+		return false
+
+
+	})
 
 });
+
+
