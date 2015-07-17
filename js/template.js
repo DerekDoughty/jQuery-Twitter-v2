ca689420498489c35e509d0817ae030de66e13b6
DerekDoughty <Doughty326@gmail.com>
2015-07-16 19:42:01 -0700

    added tweets, compose, and thread templates to template file.

--- js/template.js
+++ js/template.js
@@ -1,4 +1,12 @@
 'use strict';
 
 var Handlebars = require('hbsfy/runtime');
-var tweet = require('../templates/tweet.handlebars')
+var tweet = require('../templates/tweet.handlebars');
+var thread = require('../templates/thread.handlebars');
+var compose = require('../templates/compose.handlebars');
+
+module.exports = {
+	tweet: tweet,
+	thread: thread,
+	compose: compose
+}
