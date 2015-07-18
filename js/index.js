7fd428969fd102c40a62db97f09ae37e20189bea
DerekDoughty <Doughty326@gmail.com>
2015-07-17 20:15:40 -0700

    fixed variable names and function errors

--- js/index.js
+++ js/index.js
@@ -3,69 +3,100 @@ var $ = require ('jquery');
 var tmpl = require ('./template.js')
 
 var currentUser = {
-  handle: '@DirtyD',
-  img: 'derekfire.jpg',
+  handle: '@DerekDoughty',
+  img: '../images/derekfire.jpg',
   id: 1
 };
-
-
 
 var usersUrl = 'http://localhost:3000/users'
 var tweetsUrl = 'http://localhost:3000/tweets'
 var repliesUrl = 'http://localhost:3000/replies'
 
-
- function getUsers(){
+function getUsers(){
  	console.log('hey')
  	return $.get(usersUrl)
  	.done(function (getUsersFromUserUrl){
  		console.log(getUsersFromUserUrl)
  	})
- }
+}
+
 getUsers()
 
- function getTweets(){
+function getTweets(){
  	console.log('tweets')
  	return $.get(tweetsUrl)
  	.done(function (getTweetsFromTweetsUrl){
  		console.log(getTweetsFromTweetsUrl)
  	})
- }
+}
+
 getTweets()
 
+function getReplies(){
+	console.log('replies')
+	return $.get(repliesUrl)
+	.done(function (getRepliesFromRepliesUrl){
+		console.log(getRepliesFromRepliesUrl)
+	})
+}
 
-	function renderCompose () {
+getReplies()
 
-		return tmpl.compose()
-
-	}
-
-$(function () {
 	console.log('im loaded')
 
+function renderTweet (user, message) {
+	var idTweet = 2;
+	var fields = {
+		user: user,
+		message: message,
+		idTweet: idTweet
+	}
+	return tmpl.tweet(fields)
+}
+
+function renderThread (user, message, replies, idTweet) {
+
+	var tweetHtml = renderTweet(user, message, idTweet)
+	var fields = {
+		originalTweet: tweetHtml,
+		compose: tmpl.compose(),
+	}
+
+	return tmpl.thread(fields)
+}
+
+function renderCompose () {
+
+	return tmpl.compose()
+
+}
+
+$(function () {
+
+	console.log('doc-ready')
 
 	$('#main').on('click', 'textarea', function () {
 		$(this).parent('form').toggleClass('expand')
 			console.log('whats up')
 	})
 
-	$('#tweet').on('click', '.tweet', function () {
+	$('#tweets').on('click', '.tweet', function () {
 		
 		$(this).parents('.thread').toggleClass('expand')
 
 	})
 
 
-	$('main').on('click', 'button', function () {
+	$('#main').on('click', 'button', function () {
 		var message = $(this).parents('.compose').find('textarea').val()
 
 		if ($(this).parents('.replies').length) {
-			console.log($(this))
-			$(this).parents('.replies').append(renderTweet(User, message))
+			console.log(renderTweet(currentUser, message))
+			$(this).parents('.replies').append(renderTweet(currentUser, message))
 		
 		} else {
 		
-			$('.tweets').append(renderThread(User, message))
+			$('#tweets').append(renderThread(currentUser, message))
 			
 		}
 
@@ -77,7 +108,4 @@ $(function () {
 
 
 	})
-
 });
-
-
